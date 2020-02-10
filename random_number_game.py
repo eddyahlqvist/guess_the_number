a8d1ae6bcb52bd43ecb103c41fa1814030a5caa6
Eddy Ahlqvist <eddy.ahlqvist@gmail.com>
2020-02-10 20:16:49 +0100

    Used Numbers
    Added the display of used numbers feature

--- random_number_game.py
+++ random_number_game.py
@@ -15,6 +15,9 @@ tries = 0
 # Stores the user name from an input
 user_name = str(input("Enter your name: "))
 
+# Stores all the guessed numbers for display when the game is finished
+used_numbers = []
+
 # Sets the 'win condition'. Used in the while loop below
 win = False
 
@@ -22,8 +25,10 @@ while not win:  # Same as while win == False
     try:
         guess = int(input("Guess on a number between 1 and 100: "))
         tries += 1
+        used_numbers.append(guess)
         if guess > 100 or guess < 1:
             tries -= 1
+            used_numbers.pop()
             raise ValueError
     except ValueError:
         print("Incorrect input. Please try again.")
@@ -31,7 +36,7 @@ while not win:  # Same as while win == False
     if guess == random_number:
         print("Congratulations " + user_name + "! " + str(random_number) + " was the correct number :)")
         if tries > 1:
-            print("You guessed " + str(tries) + " times")
+            print("You guessed " + str(tries) + " times on the following numbers: " + str(used_numbers))
         else:
             print("OMG FIRST TRY! YOU ARE AMAZING!!!")
         win = True
@@ -44,18 +49,18 @@ f = open("highscore.txt", "a")
 f.write(str(tries) + " tries. User: " + user_name + "\n")
 f.close()
 
-f = open("highscore.txt", "r")
-scores = []
-for line in f.readlines():
-    scores.append([line])
-scores.sort()
-f.close()
+# f = open("highscore.txt", "r")
+# scores = []
+# for line in f.readlines():
+#     scores.append([line])
+# scores.sort()
+# f.close()
 
 # with open("highscore.txt", "w") as f:
 #     f.write(str(scores) + "\n")
 # f.close()
 
-print(scores)
+# print(scores)
 
 # f = open("highscore.txt", "r")
 # print(f.read())
